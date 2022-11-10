a939089747d34e61445e16fbdac4fc167956bbe1
Admin <Admin@DESKTOP-POGK1VB>
2022-11-10 17:01:16 +0530

    Assignment5

---
+++ nimadegana/src/Assignment5/Excep.java
@@ -0,0 +1,48 @@
+package Assignment5;
+import java.util.*;
+import java.util.Scanner;
+
+public class Excep {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+
+
+	     // Creating array to store elements
+
+	     int arr[] = new int[5];
+
+
+	     // creating and initializing variable with 0
+
+	     int i = 0;
+
+
+	     // Condition check
+
+	     while (i < 5) {
+
+
+	         // Storing user defined elements in array
+
+	         arr[i++] = s.nextInt();
+
+	     }
+
+	     System.out.println(
+
+	         "Array elements are as follows: ");
+
+
+	     // Iteration over elements
+
+	     for (int j = 0; j < 5; j++)
+
+	         System.out.print(arr[j] + "  ");
+
+	 
+	
+	}
+
+}
---
+++ nimadegana/src/Assignment5/Letter.java
@@ -0,0 +1,16 @@
+package Assignment5;
+import java.lang.*;
+
+public class Letter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="HeLlo";  
+		String s1lower=s1.toLowerCase(); 
+		String s1upper=s1.toUpperCase();
+		System.out.println(s1lower);
+		System.out.println(s1upper);
+		 
+	}
+
+}
